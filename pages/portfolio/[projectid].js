50f313583b2eb468e04b3a734f7d474f7da3b611
JKKIM <jkesonjk@gmail.com>
2023-01-06 11:21:58 +0900

    #1 TIL: 동적 경로 세그먼트 접근 방법

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,15 @@
+import router, { useRouter } from "next/router";
+
 const PortfolioProjectPage = () => {
+  const router = useRouter();
+
+  console.log("router.pathname: ", router.pathname);
+  console.log("router.query: ", router.query);
+  console.log("router.query.: ", router.query.projectid);
+
+  // send a request to some backend server
+  // to fetch the piece of data with an id of router.query.projectid
+
   return (
     <div>
       <h1>The Portfolio Project Page</h1>
